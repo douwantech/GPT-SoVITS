daf2780f96dceada11bdd9d58ce59a359c58f7ce
douwantech <douwantech@douwantech.com>
2024-06-16 01:17:01 +0000

    cog: real ref asr

--- predict_generate.py
+++ predict_generate.py
@@ -8,11 +8,28 @@ import uuid
 import requests
 import shutil
 from zipfile import ZipFile
+from funasr import AutoModel
+from tqdm import tqdm
 
 class Predictor(BasePredictor):
+    def __init__(self):
+        path_asr  = 'tools/asr/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch'
+        path_vad  = 'tools/asr/models/speech_fsmn_vad_zh-cn-16k-common-pytorch'
+        path_punc = 'tools/asr/models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch'
+        path_asr  = path_asr  if os.path.exists(path_asr)  else "iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
+        path_vad  = path_vad  if os.path.exists(path_vad)  else "iic/speech_fsmn_vad_zh-cn-16k-common-pytorch"
+        path_punc = path_punc if os.path.exists(path_punc) else "iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch"
+        self.asr_model = AutoModel(
+            model               = path_asr,
+            model_revision      = "v2.0.4",
+            vad_model           = path_vad,
+            vad_model_revision  = "v2.0.4",
+            punc_model          = path_punc,
+            punc_model_revision = "v2.0.4",
+        )
+
     def setup(self) -> None:
-        """Load the model into memory to make running multiple predictions efficient"""
-        # self.model = torch.load("./weights.pth")
+        return
 
     def predict(
         self,
@@ -26,11 +43,15 @@ class Predictor(BasePredictor):
             self.unzip_file(input_file, output_dir)
             sovites_model_path = self.find_file(output_dir, '.pth')
             gpt_model_path = self.find_file(output_dir, '.ckpt')
-            ref_wav = "denoise_opt/" + self.get_ref_wav(os.path.join(output_dir, 'asr_opt/denoise_opt.list'))
-            ref_text = "大家好，呃，最近发视频"
+            ref_wav = output_dir + "/denoise_opt/" + self.get_ref_wav(os.path.join(output_dir, 'asr_opt/denoise_opt.list'))
 
             commands = [
-                f"ffmpeg -i {output_dir}/{ref_wav} -t 3 -y ref.wav",
+                f"ffmpeg -i {ref_wav} -t 3 -y ref.wav",
+            ]
+            ref_text = self.asr_model.generate(input="ref.wav")[0]["text"]
+            print(f"Ref text: {ref_text}")
+
+            commands = [
                 f"python tools/generate.py {sovites_model_path} {gpt_model_path} ref.wav {ref_text} {text}",
             ]
 
